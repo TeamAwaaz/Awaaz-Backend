20b1e49041c1aa88b33f448891a4a7453eb1db89
academy-codex <academycodex@gmail.com>
2017-10-18 17:49:20 +0530

    API setup for signin/signup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
@@ -19,6 +20,36 @@ app.post('/signup', (req, res)=>{
     }, (error)=>{
         res.status(400).send(error);
     });
+});
+
+app.post('/signin', (req, res)=>{
+   let email = req.body.email;
+   let password = req.body.password;
+
+   User.findOne({
+        email: email,
+        password: password
+   }).then((user)=>{
+       if (user === null || user === undefined)
+           res.send({found: false});
+       else
+           res.send({
+                   user:user,
+                   found: true
+               });
+   }, (error)=>{
+       res.status(400).send(error);
+   })
+});
+
+app.get('/app/:id', (req, res)=>{
+    let userID = req.params.id;
+
+    if (ObjectID.isValid(userID)) {
+        res.send("Application page here!");
+    }else {
+        res.status(404).send("No user found!")
+    }
 });
 
 app.listen(3000, ()=>{
